edaf03c69e76f6a4171b706a7a42820d8df41df5
Wujidadi <wujidadi@gmail.com>
2022-05-07 11:53:42 +0800

    分離 helper 與特定功能輔助函數

--- CLI/PChome/FakeBpwmsRequestToAgvReply.js
+++ CLI/PChome/FakeBpwmsRequestToAgvReply.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const helper = require('./helper');
+const agvReply = require('./agvReply');
 
 const src = path.join(__dirname, './wcs_trans_log.WTL_DISPATCHER_RAW_DATA.json');
 const dst = path.join(__dirname, './Fake_AGV_Reply.json');
@@ -10,7 +11,7 @@ fs.readFile(src, 'utf-8', function(err, dataStr) {
         console.log(`讀取 ${src} 失敗`);
     } else {
         const json = JSON.parse(dataStr);
-        const replyJson = JSON.stringify(helper.buildAgvReply(json), null, 4);
+        const replyJson = JSON.stringify(agvReply.buildAgvReply(json), null, 4);
         fs.writeFile(dst, replyJson, 'utf-8', function(error) {
             if (error) {
                 console.log(`寫入 ${dst} 失敗`);
